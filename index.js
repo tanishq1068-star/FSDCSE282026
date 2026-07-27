9f2721be7b0131761d8b300d676810aedb5eacf6
tanishq1068-star <tanishq1068@gmail.com>
2026-07-27 15:38:16 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello using js")
+//console.log("hello using js")
+//function add(a, b) {
+  //  return a + b;
+//}
+//console.log(add(2, 3));
+let a=34;
+if(a>20) {
+    let a=45;
+    console.log(a);
+}
+console.log(a);
---
+++ square.js
@@ -0,0 +1,6 @@
+//squareroot of sum
+function squareRootOfSum(a, b) {
+    const sum = a + b;
+    return Math.sqrt(sum);
+}
+console.log(squareRootOfSum(4, 5));
