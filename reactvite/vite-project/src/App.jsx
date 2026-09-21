7ccb06023600ecb16ed59b804c0a982503d3279f
tanishq1068-star <tanishq1068@gmail.com>
2026-09-21 15:31:06 +0530

    REact Props ICARD

--- reactvite/vite-project/src/App.jsx
+++ reactvite/vite-project/src/App.jsx
@@ -4,20 +4,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import ICard from './components/ICard'
+import ICardGallery from './components/ICardGallery'
 
 function App() {
  
 
   return (
-    <div div style={{border:"10px solid red",height:'500px',width:'300px',backgroundColor:"white",marginTop:"100px"}}>
+   <div>
     
-    <h2 style={{color:"green",textAlign:"center"}}>Abes engineering college</h2>
-    <h3 style={{color:"blue"}}>roll number: 12345</h3>
-    <h4 style={{color:"green"}}>Name: Tanishq</h4>
-    <img src="https://www.bing.com/th/id/OIP.fnCmCPk0m964Ynm7U5Y78gAAAA?w=193&h=266&c=8&rs=1&qlt=90&o=6&dpr=2&pid=ImgAns&rm=2" style={{height:100,width:100}}/>
-    <h5 style={{color:"green",textAlign:"center"}}>branch: Computer Science and Engineering</h5>
-    <h5 style={{color:"green",textAlign:"center"}}>Programming language: JavaScript</h5>
-<ICard />
+    {/* // <h2 style={{color:"black",textAlign:"center",fontWeight:"bold"}}>Abes engineering college</h2>
+    // <h3 style={{color:"blue",fontWeight:"bold"}}>Roll number: 12345</h3>
+    // <h4 style={{color:"black",fontWeight:"bold"}}>Name: Tanishq</h4>
+    // <h5 style={{color:"black",textAlign:"center",fontWeight:"bold"}}>Course: B.Tech</h5>
+    // <img src="https://www.bing.com/th/id/OIP.fnCmCPk0m964Ynm7U5Y78gAAAA?w=193&h=266&c=8&rs=1&qlt=90&o=6&dpr=2&pid=ImgAns&rm=2" style={{height:100,width:100}}/>
+    // <h5 style={{color:"black",textAlign:"center",fontWeight:"bold"}}>Branch: Computer Science and Engineering</h5>
+    // <h5 style={{color:"black",textAlign:"center",fontWeight:"bold"}}>Programming language: JavaScript</h5>
+    // <h6 style={{color:"black",fontWeight:"bold"}}>Section: 28</h6> */}
+{/* <h2 style={{color:"black",textAlign:"center",fontWeight:"bold"}}>welcome to react vite</h2> */}
+<ICardGallery/>
     </div>
   )
 }
--- reactvite/vite-project/src/components/ICard.jsx
+++ reactvite/vite-project/src/components/ICard.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
-function ICard() {
+function ICard({data}) {
   return (
-    <div>ICard</div>
-  )
+    <div
+      style={{
+        border: "10px solid red",
+        height: "500px",
+        width: "300px",
+        backgroundColor: "white",
+        marginTop: "100px",
+        fontweight: "bold",
+        color: "black",
+      }}>
+      <img src="https://www.bing.com/th/id/OIP.fnCmCPk0m964Ynm7U5Y78gAAAA?w=193&h=266&c=8&rs=1&qlt=90&o=6&dpr=2&pid=ImgAns&rm=2"
+        style={{ height: 100, width: 100 }}
+      />
+      <h2 style={{ color: "black", textAlign: "center", fontWeight: "bold" }}>
+        roll: {data.roll}
+      </h2>
+      <h3>name: {data.name}</h3>
+      <h4>course: {data.course}</h4>
+      <h5>branch: {data.branch}</h5>
+      <h6>college: {data.college}</h6>
+    </div>
+  );
 }
 
 export default ICard
